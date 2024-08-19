c7c5c359521717b44bdda73ac2100e3ad4a057ca
Simon Crabb <simoncrabb09@gmail.com>
2024-08-18 21:20:18 -0400

    Update login.js

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+const usernameField = Document.querySelector("#Username")
+const passwordField = Document.querySelector("#Password")
+
+const accountSubmit = Document.querySelector("#submitAccount")
+
+accountSubmit.addEventListener("click", accountSubmit())
+
+function accountSubmit() {
+
+    let username = usernameField.innerHTML
+    let password = passwordField.innerHTML
+
+    
+
+}
+
